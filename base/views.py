ab60de0a545d2ebe49ad25ed6a132c8d2536be5a
Azizabonu <azibonu19@gmail.com>
2021-11-12 11:37:57 +0500

    restricted pages

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from .forms import RoomForm
@@ -8,7 +9,12 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 # Create your views here.
 
+
 def loginPage(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('username')  # User enters username
         password = request.POST.get('password')  # User enters password
@@ -30,9 +36,11 @@ def loginPage(request):
     }
     return render(request, 'base/login_register.html', context)
 
+
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -55,6 +63,7 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+@login_required(login_url='login') # restrict the page; put it in the CRUD functions
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -67,9 +76,14 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!!')
+
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
         if form.is_valid():
@@ -79,6 +93,7 @@ def updateRoom(request, pk):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room=Room.objects.get(id=pk)
     if request.method == 'POST':
