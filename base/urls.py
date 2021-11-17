981694284129f8a620da1ac846a7c114da606e7c
Azizabonu <azibonu19@gmail.com>
2021-11-17 22:25:18 +0500

    User Account page is edit and theme is installed

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
     path('register/', views.registerPage, name='register'),
+    path('update-user/', views.updateUser, name='update-user'),
 
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from .models import Room, Topic, Message
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -184,3 +184,18 @@ def deleteMessage(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': message})
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    context = {
+        'form': form,
+    }
+    return render(request, 'base/update-user.html', context)
