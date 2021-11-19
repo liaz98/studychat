6a5d993114284030a90e3621365ae0ab018a1fd5
Azizabonu <azibonu19@gmail.com>
2021-11-19 13:45:58 +0500

    Add

--- studybud/settings.py
+++ studybud/settings.py
@@ -148,6 +148,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOW_ALL_ORIGINS = True
