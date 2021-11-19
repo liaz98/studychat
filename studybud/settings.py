61bf13e7bb583dbb67b100c32f8a26885cd41672
Azizabonu <azibonu19@gmail.com>
2021-11-19 10:15:46 +0500

    Updated

--- studybud/settings.py
+++ studybud/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -136,9 +136,10 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-# STATIC_ROOT = [
-#
-# ]
+
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'static/images')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
